baf760785336139819df889d7eb0a45fd2d4d9b2
Laksh-Koundinya <lakshmisn.08@gmail.com>
2020-10-10 18:58:04 +0530

    Completed Signup

---
+++ signup/signup.js
@@ -0,0 +1,43 @@
+var arr;
+function loadData(key){
+    return JSON.parse(localStorage.getItem(key))
+}
+
+
+function saveData(key,data){
+localStorage.setItem(key, JSON.stringify(data))
+}
+
+window.addEventListener('load', function(){
+  alert('works')
+    arr = loadData('customerdetail') || []  
+    var form = document.getElementById("form")
+    form.addEventListener("submit",handleData)
+   
+    
+})
+
+function handleData(){
+    event.preventDefault()
+    var form = new FormData(event.target)
+    var mail = form.get("mail")
+    
+    var name = form.get("name")
+    var pass = form.get("password")
+    var found = arr.some(function(el){
+        return el.email === mail;
+  });
+    if (!found) { 
+      arr.push({name:name, email: mail, password:pass  });
+     }
+     else {
+         alert("User Email Already exists try Login")
+     }
+    document.getElementById("form").reset()
+   
+   
+    saveData('customerdetail', arr) 
+    //console.log(arr)
+}
+
+
