18175d8e65c2e9fc0e9227e0e56463724cff20b8
Laksh-Koundinya <lakshmisn.08@gmail.com>
2020-10-11 14:03:20 +0530

    Login Page Complete

--- login/login.js
+++ login/login.js
@@ -4,13 +4,28 @@ window.addEventListener("load", function(){
   })
 
 function enterLoginDetails() {
+    var arr = JSON.parse(localStorage.getItem('customerdetail'))
+    console.log(arr)
     event.preventDefault();
     var form = new FormData(event.target);
     var email = form.get("email");
     var password = form.get("password");
-    console.log(loadData("customerdetail")); 
+    var check = false;
+    for(var i = 0; i<arr.length; i++){
+        //console.log(arr[i].email)
+       if(email === arr[i].email && password === arr[i].password) {
+           check = true;
+           break;
+       }
+    }
+    if(check){
+        console.log("VAlid")
+
+        location.assign("file:///C:/Users/lenovo/Desktop/Masai_pair_peoject/expense-manager/signup/signup.html")
+    }else {
+        var showError = document.getElementById('notify')
+        showError.textContent = "Username or Password Incorrect"
+    }
+    
 }
 
-function loadData(key){
-    return JSON.parse(localStorage.getItem(key));
-}
